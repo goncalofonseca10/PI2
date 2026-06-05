92677c4cec471a6f7a5940dfb230fdcb79262939
goncalo <goncalofonseca10@gmail.com>
2026-06-05 17:54:47 +0100

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,32 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\KitController;
+use App\Http\Controllers\Api\CategoriaController;
+use App\Http\Controllers\Api\ItemController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function() {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('user', [AuthController::class, 'user']);
+    
+    Route::get('/kits', [KitController::class, 'index']);
+    Route::post('/kits', [KitController::class, 'store']);
+    Route::get('/kits/{id}', [KitController::class, 'show']);
+    Route::put('/kits/{id}', [KitController::class, 'update']);
+    Route::delete('/kits/{id}', [KitController::class, 'destroy']);
+
+    Route::get('/categorias', [CategoriaController::class, 'index']);
+    Route::post('/categorias', [CategoriaController::class, 'store']);
+    Route::put('/categorias/{id}', [CategoriaController::class, 'update']);
+    Route::delete('/categorias/{id}', [CategoriaController::class, 'destroy']);
+
+    Route::get('/items', [ItemController::class, 'index']);
+    Route::post('/items', [ItemController::class, 'store']);
+    Route::put('/items/{id}', [ItemController::class, 'update']);
+    Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+
+    });
